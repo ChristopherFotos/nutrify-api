2eed481ef7cf6d726dc94839899ec4028f543a60
dreamgirlto <38843981+dreamgirlto@users.noreply.github.com>
2020-06-25 12:59:10 -0400

    The Nutrify API

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const mongoose = require('mongoose')
+app.use(cors());
+
+
+console.log('PASSWORD: ', process.env.MONGO_ATLAS_PW, "SECRET_KEY: ", process.env.MONGO_ATLAS_PW)
+
+// Connecting to the Mongo Atlas database                                         
+mongoose.connect('mongodb+srv://chrisfotos:sed1sed1@node-rest-shop-7baom.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true })
+
+mongoose.Promise = global.Promise;
+
+
+app.set('view engine', 'ejs')
+
+// Set up body parser and Morgan, other middlewear
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'))
+app.use(express.static('public'))
+app.use(express.static('public/build'))
+
+// Handling CORS errors and OPTIONS request
+
+app.use(cors());
+
+// Set up routes
+app.use("/user", userRoutes)
+
+// Set up view routes
+app.get('/login', (req, res, next) => {
+    res.render('login')
+}
+)
+
+// Error handling
+app.use((req, res, next) => {
+    const error = new Error('not found');
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
+module.exports = app;
